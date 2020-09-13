6bb1738b2b599399d8b181e3b67343f206ed440a
Dan Ruswick <dan.ruswick@tradingtechnologies.com>
2020-09-13 17:54:29 -0500

    cleanup

--- readme.md
+++ readme.md
@@ -2,25 +2,26 @@
 ## Automatically Upload Photos in a Folder to Google Photos
 
 ## How it works
-Photo Drop creates a cron job that checks on your directory every one minute. If it detects anything with the extension .jpg, .jpeg, .png or .gif, it will upload it to your google photos account and DELETE IT
+Photo Drop creates a cron job that checks on your directory every one minute. If it detects anything with the extension .jpg, .jpeg, .png or .gif, it will upload it to your google photos account and DELETE IT.
 
 ## Why does it delete the photos?
-I wrote this to be a quick and easy way to get photos from my macbook (mostly memes I save from reddit) into google photos. Uploading and then deleting them is the use case I was trying to solve for. If you want a full service sync solution, use dropbox or something designed for sync...
+I wrote this to be a quick and easy way to get photos from my macbook (mostly memes I save from reddit) into Google Photos. Uploading and then deleting them is the use case I was trying to solve for. If you want a full service sync solution, use dropbox or something designed for sync...
 
 ## How do I know this is safe?
 Well it's open source and the code is pretty simple. Feel free to dig into it yourself.
 
 
-##Setup
+## Setup
 
 1. Clone the repo
-2. Run `python google_photo_drop/setup.py` (requires python >=3.7)
-3. When prompted, give the app permission to create the cron job
-4. When prompted, log in with google
-5. When prompted, enter a default location for your folder
-6. IF YOU ARE ON MAC OS CATALINA: for most folder locations (including desktop), you will need to give cron full disk access, which is annoying. Sorry, but Apple hates power users...
-7. Go to `System Preferences > Security & Privacy > Privacy > Full Disk Access`
-8. Click the padlock to make a change
-8. Hit the `+` button
-9. Hit `⌘ + ⇧ + g` (command + shift + g) and enter /user/sbin
-10. Select `cron` and then click `Open`
+2. Run `pip intall -r google_photo_drop/requirements.txt`
+3. Run `python google_photo_drop/setup.py` (requires python >=3.7)
+4. When prompted, give the app permission to create the cron job
+5. When prompted, log in with Google
+6. When prompted, enter a default location for your folder
+7. IF YOU ARE ON MAC OS CATALINA: for most folder locations (including `~/Desktop/*`), you will need to give cron full disk access, which is annoying. Sorry, but Apple hates power users...
+8. Go to `System Preferences > Security & Privacy > Privacy > Full Disk Access`
+9. Click the padlock to make a change
+10. Hit the `+` button
+11. Hit `⌘ + ⇧ + g` (command + shift + g) and enter `/user/sbin`
+12. Select `cron` and then click `Open`
--- upload.py
+++ upload.py
@@ -24,46 +24,59 @@ with open(lock, 'w+'):
 try:
     with open(os.path.join(path, 'creds.json'), 'r') as file:
         creds = json.load(file)
-        upload_dir = creds.get('loc')
-        bearer = {'Authorization': 'Bearer ' + creds.get('access_token')}
-        print(creds)
-        # https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
-        files = [f for f in os.listdir(upload_dir) if os.path.isfile(os.path.join(upload_dir, f))]
-        print(files)
-        images = [os.path.join(upload_dir, f) for f in files if '.jpg' in f or '.jpeg' in f or '.png' in f or '.gif' in f]
-        print(images)
-        for i in images:
-            mime = 'image/' + i.split('.')[-1].replace('jpg', 'jpeg')
-            # https://stackoverflow.com/questions/45868120/python-post-request-with-bearer-token
-            # https://stackoverflow.com/questions/39614675/sending-raw-data-in-python-requests
-            head = {**bearer,
-            'Content-type': 'application/octet-stream',
-            'X-Goog-Upload-Content-Type': mime,
-            'X-Goog-Upload-Protocol': 'raw'
-            }
-            with open(i, 'rb') as image_file:
-                # https://developers.google.com/photos/library/guides/upload-media
-                resp = requests.post('https://photoslibrary.googleapis.com/v1/uploads', data=image_file, headers=head)
-                token = resp.text
-                print(token)
-                payload = {
-                    "newMediaItems": [
-                        {
+    if 'loc' not in creds or 'access_token' not in creds:
+        print('creds are invalid')
+        sys.exit()
+
+    upload_dir = creds.get('loc')
+    bearer = {'Authorization': 'Bearer ' + creds.get('access_token')}
+    print(creds)
+
+
+    # https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+    files = [f for f in os.listdir(upload_dir) if os.path.isfile(os.path.join(upload_dir, f))]
+    images = [os.path.join(upload_dir, f) for f in files if '.jpg' in f or '.jpeg' in f or '.png' in f or '.gif' in f]
+    print(images)
+
+
+
+    for i in images:
+        
+        # https://stackoverflow.com/questions/45868120/python-post-request-with-bearer-token
+        # https://stackoverflow.com/questions/39614675/sending-raw-data-in-python-requests
+        head = {**bearer,
+        'Content-type': 'application/octet-stream',
+        'X-Goog-Upload-Content-Type': mime,
+        'X-Goog-Upload-Protocol': 'raw'
+        }
+        mime = 'image/' + i.split('.')[-1].replace('jpg', 'jpeg')
+
+
+        with open(i, 'rb') as image_file:
+            # https://developers.google.com/photos/library/guides/upload-media
+            resp = requests.post('https://photoslibrary.googleapis.com/v1/uploads', data=image_file, headers=head)
+            token = resp.text
+
+
+            payload = {
+                "newMediaItems": [
+                    {
                         "description": i,
                         "simpleMediaItem": {
                             "fileName": i,
                             "uploadToken": token
                         }
-                        }
-                    ]
                     }
-                resp = requests.post('https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate', json=payload, headers=bearer)
-                try:
-                    print(resp.json())
-                except:
-                    print(resp.text)
-                
-                os.remove(i)
+                ]
+            }
+            resp = requests.post('https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate', json=payload, headers=bearer)
+            
+            try:
+                print(resp.json())
+            except:
+                print(resp.text)
+            
+            os.remove(i)
                 
 except Exception as e:
     print(e)
