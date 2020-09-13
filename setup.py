3e5270b03a2a38f879b2b18c4ceda8bdd8c57bea
Dan Ruswick <dan.ruswick@tradingtechnologies.com>
2020-09-13 18:17:18 -0500

    added refresh token

--- setup.py
+++ setup.py
@@ -13,33 +13,6 @@ import requests
 import json
 import urllib.parse
 
-print("\n\n\n1. CRON SETUP")
-print('We are going to set up a cron job to run the uploader script every 1 minute')
-print('(please select allow if prompted)\n\n\n')
-input('Press any key to continue...')
-
-
-path = pathlib.Path(__file__).parent.absolute()
-python_path = sys.executable
-upload_path = os.path.join(path, 'upload.py')
-
-# https://unix.stackexchange.com/questions/297374/add-something-to-crontab-programmatically-over-ssh
-cron_command = f'''echo "$(echo '* * * * * {python_path} {upload_path} > /tmp/google_photo_drop.log' ; crontab -l | grep -v '{python_path} {upload_path}')" | crontab -'''
-
-subprocess.run(cron_command, shell=True)
-
-print("\n\n\n2. Token Fetch")
-print('Next, we are going to get you a token from google photos using OAuth.')
-print('To do that, we will briefly open a local web server and direct you to the google oauth flow')
-print('\n\n\n')
-input('Press any key to continue...')
-
-
-p = Process(target=server.main)
-p.start()
-pid = p.pid
-
-
 CLIENT_ID = '616621932811-in59kvi1eao9mfncbdfg64pa4k0mvj8t.apps.googleusercontent.com'
 # secrets are not actually secret in installed apps and that's fine
 # https://stackoverflow.com/questions/20725062/oauth-secrets-and-desktop-application
@@ -51,89 +24,121 @@ REDIRECT_URL = urllib.parse.quote('http://localhost:5000/auth')
 CODE_CHALLENGE = str(uuid.uuid4())
 STATE = str(int(1000000000* random.random()))
 
-# https://developers.google.com/identity/protocols/oauth2
-# https://developers.google.com/identity/protocols/oauth2/native-app#create-code-challenge
-endpoint = f'https://accounts.google.com/o/oauth2/v2/auth?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URL}&response_type=code&scope={SCOPES}&code_challenge={CODE_CHALLENGE}&code_challenge_method=plain&state={STATE}'
+def main():
+
+    print("\n\n\n1. CRON SETUP")
+    print('We are going to set up a cron job to run the uploader script every 1 minute')
+    print('(please select allow if prompted)\n\n\n')
+    input('Press any key to continue...')
 
 
-webbrowser.open(endpoint)
+    path = pathlib.Path(__file__).parent.absolute()
+    python_path = sys.executable
+    upload_path = os.path.join(path, 'upload.py')
 
-auth = None
-now = time.time()
-subprocess.run('rm /tmp/photo_drop_auth.json', shell=True)
-subprocess.run(f'rm {os.path.join(path, "creds.json")}', shell=True)
-try:
-    while True:
-        # https://www.guru99.com/python-check-if-file-exists.html
-        if os.path.exists('/tmp/photo_drop_auth.json'):
-            with open('/tmp/photo_drop_auth.json', 'r') as file:
-                # https://stackoverflow.com/questions/39719689/what-is-the-difference-between-json-load-and-json-loads-functions
-                auth = json.load(file)
-                os.remove('/tmp/photo_drop_auth.json')
-                break
-        if time.time() > now + 90:
-            break
-        time.sleep(1)
-except:
-    pass
-finally:
-    os.kill(pid, signal.SIGINT)
+    # https://unix.stackexchange.com/questions/297374/add-something-to-crontab-programmatically-over-ssh
+    cron_command = f'''echo "$(echo '* * * * * {python_path} {upload_path} > /tmp/google_photo_drop.log' ; crontab -l | grep -v '{python_path} {upload_path}')" | crontab -'''
 
-if not auth:
-    print('\n\n\nLOOKS LIKE THE FETCH TIMED OUT! PLEASE TRY AGAIN...\n\n\n')
-    sys.exit()
+    subprocess.run(cron_command, shell=True)
 
-if auth.get('state') != STATE:
-    print('\n\n\nTHERE WAS AN ISSUE FETCHING YOUR TOKEN! PLEASE TRY AGAIN...\n\n\n')
-    sys.exit()
+    print("\n\n\n2. Token Fetch")
+    print('Next, we are going to get you a token from google photos using OAuth.')
+    print('To do that, we will briefly open a local web server and direct you to the google oauth flow')
+    print('\n\n\n')
+    input('Press any key to continue...')
 
-endpoint = 'https://oauth2.googleapis.com/token'
-data = {
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET,
-    'redirect_uri': 'http://localhost:5000/auth',
-    'grant_type': 'authorization_code',
-    'code': auth.get("code"),
-    'code_verifier': CODE_CHALLENGE
-}
 
-# https://www.kite.com/python/examples/1607/requests-send-a-post-request-with-form-data
-req = requests.post(endpoint, data)
-creds = None
-try:
-    creds = req.json()
-except:
-    print('\n\n\nLOOKS LIKE THE CREDS ARE INVALID! PLEASE TRY AGAIN...\n\n\n')
-    sys.exit()
+    p = Process(target=server.main)
+    p.start()
+    pid = p.pid
 
-if 'access_token' not in creds:
-    print('\n\n\nLOOKS LIKE THE CREDS ARE INVALID! PLEASE TRY AGAIN...\n\n\n')
-    sys.exit()
 
-print("\n\n\n3. DRIVE LOCATION")
-print('Enter a location for your drop dir')
-p = input('Location (defaults to ~/Desktop/GooglePhotoDrop): ')
-p = p.replace('_', '')
-if len(p) == 0:
-    p = '~/Desktop/GooglePhotoDrop'
-p = os.path.expanduser(p)
+    # https://developers.google.com/identity/protocols/oauth2
+    # https://developers.google.com/identity/protocols/oauth2/native-app#create-code-challenge
+    endpoint = f'https://accounts.google.com/o/oauth2/v2/auth?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URL}&response_type=code&scope={SCOPES}&code_challenge={CODE_CHALLENGE}&code_challenge_method=plain&state={STATE}'
 
-if os.path.exists(p):
-    print(f'path {p} already exists, so we will use this existing dir')
-else:
-    print(f'creating {p}...')
+
+    webbrowser.open(endpoint)
+
+    auth = None
+    now = time.time()
+    subprocess.run('rm /tmp/photo_drop_auth.json', shell=True)
+    subprocess.run(f'rm {os.path.join(path, "creds.json")}', shell=True)
     try:
-        os.makedirs(p)
+        while True:
+            # https://www.guru99.com/python-check-if-file-exists.html
+            if os.path.exists('/tmp/photo_drop_auth.json'):
+                with open('/tmp/photo_drop_auth.json', 'r') as file:
+                    # https://stackoverflow.com/questions/39719689/what-is-the-difference-between-json-load-and-json-loads-functions
+                    auth = json.load(file)
+                    os.remove('/tmp/photo_drop_auth.json')
+                    break
+            if time.time() > now + 90:
+                break
+            time.sleep(1)
     except:
+        pass
+    finally:
+        os.kill(pid, signal.SIGINT)
+
+    if not auth:
+        print('\n\n\nLOOKS LIKE THE FETCH TIMED OUT! PLEASE TRY AGAIN...\n\n\n')
+        sys.exit()
+
+    if auth.get('state') != STATE:
+        print('\n\n\nTHERE WAS AN ISSUE FETCHING YOUR TOKEN! PLEASE TRY AGAIN...\n\n\n')
+        sys.exit()
+
+    endpoint = 'https://oauth2.googleapis.com/token'
+    data = {
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'redirect_uri': 'http://localhost:5000/auth',
+        'grant_type': 'authorization_code',
+        'code': auth.get("code"),
+        'code_verifier': CODE_CHALLENGE
+    }
+
+    # https://www.kite.com/python/examples/1607/requests-send-a-post-request-with-form-data
+    req = requests.post(endpoint, data)
+    creds = None
+    try:
+        creds = req.json()
+    except:
+        print('\n\n\nLOOKS LIKE THE CREDS ARE INVALID! PLEASE TRY AGAIN...\n\n\n')
+        sys.exit()
+
+    if 'access_token' not in creds:
+        print('\n\n\nLOOKS LIKE THE CREDS ARE INVALID! PLEASE TRY AGAIN...\n\n\n')
+        sys.exit()
+
+    print("\n\n\n3. DRIVE LOCATION")
+    print('Enter a location for your drop dir')
+    p = input('Location (defaults to ~/Desktop/GooglePhotoDrop): ')
+    p = p.replace('_', '')
+    if len(p) == 0:
+        p = '~/Desktop/GooglePhotoDrop'
+    p = os.path.expanduser(p)
+
+    if os.path.exists(p):
+        print(f'path {p} already exists, so we will use this existing dir')
+    else:
+        print(f'creating {p}...')
+        try:
+            os.makedirs(p)
+        except:
+            print(F'\n\n\nPATH {p} COULD NOT BE CREATED! PLEASE TRY AGAIN...\n\n\n')
+            sys.exit()
+
+    if not os.path.exists(p):
         print(F'\n\n\nPATH {p} COULD NOT BE CREATED! PLEASE TRY AGAIN...\n\n\n')
         sys.exit()
 
-if not os.path.exists(p):
-    print(F'\n\n\nPATH {p} COULD NOT BE CREATED! PLEASE TRY AGAIN...\n\n\n')
-    sys.exit()
 
 
-
-with open(os.path.join(path, 'creds.json'), 'w+') as file:
-    creds['loc'] = p
-    file.write(json.dumps(creds))
+    with open(os.path.join(path, 'creds.json'), 'w+') as file:
+        creds['loc'] = p
+        file.write(json.dumps(creds))
+        
+if __name__ == "__main__":
+    main()
--- upload.py
+++ upload.py
@@ -4,43 +4,79 @@ import pathlib
 import sys
 import json
 import requests
+from setup import CLIENT_ID, CLIENT_SECRET
 
 print(time.time())
 
 path = pathlib.Path(__file__).parent.absolute()
 lock = os.path.join(path, '.lock')
 
+
+# need to make sure creds.json is there
 if not os.path.exists(os.path.join(path, 'creds.json')):
     print('creds not present, skipping')
     sys.exit()
 
+# if the lock is present, don't start another job
 if os.path.exists(lock):
     print('upload still happening, will skip this round')
     sys.exit()
 
+# create the lock
 with open(lock, 'w+'):
     pass
 
 try:
+    # load the creds
     with open(os.path.join(path, 'creds.json'), 'r') as file:
         creds = json.load(file)
+    
+    # check to see if it's malformed
     if 'loc' not in creds or 'access_token' not in creds:
         print('creds are invalid')
         sys.exit()
+    
+    # refresh the token
+    head = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = {
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'refresh_token': creds.get('refresh_token'),
+        'grant_type': 'refresh_token'
 
+    }
+    resp = requests.post('https://oauth2.googleapis.com/token', data, headers=head)
+    token = resp.json().get('access_token')
+    print(resp.json())
+
+    # make sure the refreshed token is valid
+    if not token:
+        print('failed to refresh your token')
+        sys.exit()
+
+    # update the loc for these new creds
+    new_creds = resp.json()
+    new_creds['loc'] = creds.get('loc')
+
+    # store the new creds in the file
+    with open(os.path.join(path, 'creds.json'), 'w') as file:
+        file.write(json.dumps(new_creds))
+
+    # now we have our location and token
     upload_dir = creds.get('loc')
     bearer = {'Authorization': 'Bearer ' + creds.get('access_token')}
-    print(creds)
 
 
+    # get a list of files to upload
     # https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
     files = [f for f in os.listdir(upload_dir) if os.path.isfile(os.path.join(upload_dir, f))]
     images = [os.path.join(upload_dir, f) for f in files if '.jpg' in f or '.jpeg' in f or '.png' in f or '.gif' in f]
     print(images)
 
 
-
+    # iterate over them
     for i in images:
+        # figure out the mime type
         mime = 'image/' + i.split('.')[-1].replace('jpg', 'jpeg')
         # https://stackoverflow.com/questions/45868120/python-post-request-with-bearer-token
         # https://stackoverflow.com/questions/39614675/sending-raw-data-in-python-requests
@@ -50,14 +86,16 @@ try:
         'X-Goog-Upload-Protocol': 'raw'
         }
 
-        # TODO need to handle refresh token!
-
+        # open the file AS BINARY
         with open(i, 'rb') as image_file:
+            # first we upload the raw bits
             # https://developers.google.com/photos/library/guides/upload-media
             resp = requests.post('https://photoslibrary.googleapis.com/v1/uploads', data=image_file, headers=head)
+
+            # google gives us back a token (so many tokens!)
             token = resp.text
 
-
+            # now we create a "media item" which is google parlance for an image that shows up in a users library
             payload = {
                 "newMediaItems": [
                     {
@@ -70,17 +108,15 @@ try:
                 ]
             }
             resp = requests.post('https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate', json=payload, headers=bearer)
-
-            try:
-                print(resp.json())
-            except:
-                print(resp.text)
             
+            # delete this photo
             os.remove(i)
                 
 except Exception as e:
     print(e)
+    raise
 finally:
+    # we are done, so remove the lock
     os.remove(lock)
 
 
